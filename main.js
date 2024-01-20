25da6dc25bd469926315ab73f17c81b6286acdeb
Ryan Seiyu <ryan_seiyu@hotmail.com>
2024-01-19 23:25:37 -0500

    atualizando

--- main.js
+++ main.js
@@ -2,33 +2,28 @@ window.onload = function () {
 	document.getElementById("botao2").click();
 };
 
-const pronounsHeader = document.getElementById("pronounsHeader");
-const pronouns = document.getElementById("pronouns").textContent;
-const languages = document.getElementById("languagesHeader").textContent;
+const pronounsHeaderPort =
+	document.getElementById("pronounsHeader").textContent;
+const pronounsPort = document.getElementById("pronouns").textContent;
+const languagesPort = document.getElementById("languagesHeader").textContent;
 const descriptionPort = document.getElementById("description").textContent;
 const header1Port = document.getElementById("header1").textContent;
 const header2Port = document.getElementById("header2").textContent;
-const header2_1Port = document.getElementById("header2_1").textContent;
 const header2_4Port = document.getElementById("header2_4").textContent;
-const description2_1Port =
-	document.getElementById("description2_1").textContent;
-const description2_2Port =
-	document.getElementById("description2_2").textContent;
 const description2_3Port =
 	document.getElementById("description2_3").textContent;
 const description2_4Port =
 	document.getElementById("description2_4").textContent;
 const header3Port = document.getElementById("header3").textContent;
 const header3_1Port = document.getElementById("header3_1").textContent;
+const description3_1Port =
+	document.getElementById("description3_1").textContent;
 const description3_2Port =
 	document.getElementById("description3_2").textContent;
 const description3_3Port =
 	document.getElementById("description3_3").textContent;
-const header4Port = document.getElementById("header4").textContent;
-const header4_1Port = document.getElementById("header4_1").textContent;
-const description4_1Port =
-	document.getElementById("description4_1").textContent;
 const header5Port = document.getElementById("header5").textContent;
+const CompanyLabelPort = document.getElementById("companyLabel").textContent;
 const header5_subtitle1Port =
 	document.getElementById("header5_subtitle1").textContent;
 const description5_1Port =
@@ -58,6 +53,7 @@ const item6_9Port = document.getElementById("item6_9").textContent;
 const item6_10Port = document.getElementById("item6_10").textContent;
 const item6_11Port = document.getElementById("item6_11").textContent;
 const item6_12Port = document.getElementById("item6_12").textContent;
+const item6_13Port = document.getElementById("item6_13").textContent;
 const header7Port = document.getElementById("header7").textContent;
 const header8Port = document.getElementById("header8").textContent;
 
@@ -68,20 +64,16 @@ function changeLanguage(lang) {
 	let description = document.getElementById("description");
 	let header1 = document.getElementById("header1");
 	let header2 = document.getElementById("header2");
-	let header2_1 = document.getElementById("header2_1");
 	let header2_4 = document.getElementById("header2_4");
-	let description2_1 = document.getElementById("description2_1");
-	let description2_2 = document.getElementById("description2_2");
 	let description2_3 = document.getElementById("description2_3");
 	let description2_4 = document.getElementById("description2_4");
 	let header3 = document.getElementById("header3");
 	let header3_1 = document.getElementById("header3_1");
+	let description3_1 = document.getElementById("description3_1");
 	let description3_2 = document.getElementById("description3_2");
 	let description3_3 = document.getElementById("description3_3");
-	let header4 = document.getElementById("header4");
-	let header4_1 = document.getElementById("header4_1");
-	let description4_1 = document.getElementById("description4_1");
 	let header5 = document.getElementById("header5");
+	let CompanyLabel = document.getElementById("companyLabel");
 	let header5_subtitle1 = document.getElementById("header5_subtitle1");
 	let description5_1 = document.getElementById("description5_1");
 	let header5_subtitle2 = document.getElementById("header5_subtitle2");
@@ -103,47 +95,45 @@ function changeLanguage(lang) {
 	let item6_10 = document.getElementById("item6_10");
 	let item6_11 = document.getElementById("item6_11");
 	let item6_12 = document.getElementById("item6_12");
+	let item6_13 = document.getElementById("item6_13");
 	let header7 = document.getElementById("header7");
 	let header8 = document.getElementById("header8");
 
 	if (lang == "en") {
-		pronounsHeader.innerHTML = "Pronouns:";
+		pronounsHeader.innerHTML = "Pronouns -";
 		pronouns.innerHTML = "He/Him";
-		languages.innerHTML = "Languages:";
+		languages.innerHTML = "Languages -";
 		description.innerHTML =
 			"With a history dating back to my childhood in bustling Sao Paulo, I am an intrepid individual passionate about technology. From an early age, my fascination with computers and electronics led me to confront unknown challenges, disassembling and attempting to repair devices even without fully understanding their operation. Each failure was a step in my learning journey. Upon enrolling at UTFPR to study Electrical Engineering, I deepened my knowledge, embracing challenging projects like creating systems against static electricity and macros for spreadsheet optimization. My passion led me to software development, turning me into an experienced Full Stack Developer (React/Redux/Java/Python Developer). Today, I continue to explore the unknown with courage and determination, ready to face new challenges in my career. My name is Ryan Kimura.";
 		header1.innerHTML = "Stack";
 		header2.innerHTML = "Projects and Activities";
-		header2_1.innerHTML = "Board Member of Democratic Elections";
 		header2_4.innerHTML = "Development Projects";
-		description2_1.innerHTML =
-			"To provide guidance and supervision of the brazilian democratic elections, representing the regional electoral court. Exercising all functions required for the voting system to work.";
-		description2_2.innerHTML =
-			"Using AI to create images, with the Automatic1111 repository for local host execution, or Google Notebook for remote executions.";
 		description2_3.innerHTML =
 			"Development of webscraping for data collection from websites, using JavaScript and Puppeteer. More specifically, to acquire a list of doctors in the region of Toronto, Canada closest to a specific address.";
 		description2_4.innerHTML =
 			"Various website developments with frameworks, CI/CD integration, Git, and GitHub for automatic deployment, using SQL and NoSQL databases, in addition to implementing RESTful APIs. Other tools such as Docker, Postgres, MongoDB, AWS, and Heroku were also used. In addition to the application of unit tests, and projects developed with a microservices architecture.";
 		header3.innerHTML = "Education";
 		header3_1.innerHTML = "Electrical Engineering";
+		description3_1.innerHTML =
+			"The Federal University of Technology – Parana (Portuguese: Universidade Tecnologica Federal do Parana, UTFPR) UTFPR is one of the most renowned Universities in Brazil.";
 		description3_2.innerHTML =
-			"Full scholarship to be an Exchange Student at the United States for a year. Studied at Wayne State University (Detroit, MI) and New Jersey Institute of Technology (Newark, NJ).";
+			"Bachelor's degree in Computer Engineering (or similar) or specialized technical course in Computer Science.";
 		description3_3.innerHTML =
-			"Full Stack Java Development course to learn Java, HTML, CSS, Javascript, Node.js, Git and React at EBAC. Dedicated 45% of my time to learning Java and the other 50% to learning React.";
-		header4.innerHTML = "Certifications";
-		header4_1.innerHTML = "PAN Bank Java Developer";
-		description4_1.innerHTML =
-			"Certificate of completion, from DIO (Java Bootcamp course). Technical and practical learning for the development of Java applications with Spring Boot.";
-		header5.innerHTML = "Experience";
+			"Full Stack Java Development course to learn Java (Object Oriented Programming), HTML, CSS, Javascript, Node.js, Git and React (Front-end) at EBAC. Dedicated 45% of my time to learning Java and the other 50% to learning React.";
+		header5.innerHTML = "Job Experience";
+		let elements = document.querySelectorAll("#companyLabel");
+		elements.forEach((element) => {
+			element.innerHTML = "Company -&nbsp";
+		});
 		header5_subtitle1.innerHTML = "Developer";
 		description5_1.innerHTML =
 			"Development of a comprehensive Power BI course, tailored to students' needs, with extensive instructional materials.<br>I acted as a facilitator, providing individual support and guidance.<br>The course empowered students to master Power BI and received positive feedback, reinforcing my passion for empowering others in data analysis.";
 		header5_subtitle2.innerHTML = "Research Assistent";
 		description5_2.innerHTML =
-			"Analytical and technical research on nanowire light-emitting diodes.<br>Advanced simulations in Matlab for parameter optimization.<br>Successfully implemented software across the laboratory team in the United States, resulting in a significant cost reduction of over 80%.<br>Technical expertise in technology research and development.";
+			"Analytical and technical research on nanowire light-emitting diodes.<br>Advanced simulations in Matlab for parameter optimization.<br>Successfully implemented software across the laboratory team in the United States, resulting in a significant cost reduction of over 80%.<br>Technical expertise in technology research and development. Commitment and passion for learning new things.";
 		header5_subtitle3.innerHTML = "Intern";
 		description5_3.innerHTML =
-			"Marketing of electric energy, including contract negotiations.<br>Effective mediation between suppliers and customers.<br>Active and analytical 	prospecting of new clients and sales strategies.<br>Technical development of a customized Excel macros to automate the company's email sending process to its employees, resulting in a reduction of over 90% in the time required to complete this task.";
+			"Marketing of electric energy, including contract negotiations.<br>Effective mediation between suppliers and customers.<br>Communicating progress to Top Management through reports, meetings, and/or presentations.Active and analytical 	prospecting of new clients and sales strategies.<br>Technical development of a customized Excel macros to automate the company's email sending process to its employees, resulting in a reduction of over 90% in the time required to complete this task.";
 		header5_subtitle4.innerHTML = "Manager";
 		description5_4.innerHTML =
 			"Team leadership, goal setting, and management strategy definition.<br>Experience in financial accounting and tax planning.<br>High-quality graphic design for marketing.<br>Successful technical integration with government back-end systems, ensuring tax compliance, reducing costs to about 20%.";
@@ -160,27 +150,28 @@ function changeLanguage(lang) {
 		item6_10.innerHTML = "Organization";
 		item6_11.innerHTML = "Attention to detail";
 		item6_12.innerHTML = "Critical thinking";
+		item6_13.innerHTML = "Technical team";
 		header7.innerHTML = "Contact";
 		header8.innerHTML = "Social Media";
 	} else if (lang == "pt-br") {
-		pronounsHeader.innerHTML = "Pronomes:";
-		pronouns.innerHTML = "Ele/Dele";
-		languages.innerHTML = "Idiomas";
+		pronounsHeader.innerHTML = pronounsHeaderPort;
+		pronouns.innerHTML = pronounsPort;
+		languages.innerHTML = languagesPort;
 		description.innerHTML = descriptionPort;
 		header1.innerHTML = header1Port;
 		header2.innerHTML = header2Port;
-		header2_1.innerHTML = header2_1Port;
 		header2_4.innerHTML = header2_4Port;
-		description2_1.innerHTML = description2_1Port;
-		description2_2.innerHTML = description2_2Port;
 		description2_3.innerHTML = description2_3Port;
 		description2_4.innerHTML = description2_4Port;
 		header3.innerHTML = header3Port;
+		description3_1.innerHTML = description3_1Port;
 		description3_2.innerHTML = description3_2Port;
 		description3_3.innerHTML = description3_3Port;
-		header4.innerHTML = header4Port;
-		header4_1.innerHTML = header4_1Port;
-		description4_1.innerHTML = description4_1Port;
+		header5.innerHTML = header5Port;
+		let elements = document.querySelectorAll("#companyLabel");
+		elements.forEach((element) => {
+			element.textContent = CompanyLabelPort;
+		});
 		header5_subtitle1.innerHTML = header5_subtitle1Port;
 		description5_1.innerHTML = description5_1Port;
 		header5_subtitle2.innerHTML = header5_subtitle2Port;
@@ -202,6 +193,7 @@ function changeLanguage(lang) {
 		item6_10.innerHTML = item6_10Port;
 		item6_11.innerHTML = item6_11Port;
 		item6_12.innerHTML = item6_12Port;
+		item6_13.innerHTML = item6_13Port;
 		header7.innerHTML = header7Port;
 		header8.innerHTML = header8Port;
 	}
